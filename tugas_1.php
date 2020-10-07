18a369f0ebd2ca65f0cafb740de2254e6175daf6
thaerachman <71365565+thaerachman@users.noreply.github.com>
2020-10-07 10:09:41 +0700

    Add files via upload

---
+++ tugas_1.php
@@ -0,0 +1,26 @@
+<?php
+
+$ayam_goreng="Rp.13.000";
+$ayam_bakar="Rp.15.000";
+$ayam_sayur="Rp.13.000";
+$tempe_goreng="Rp.1.000";
+$tahu_goreng="Rp.1.000";
+$soto="Rp.3.000";
+$nasi_putih="Rp.5.000";
+$es_teh="Rp.4.000";
+$es_tebu="Rp.5.000";
+
+echo "<h1>Daftar Menu</h1>";
+echo "<li> Ayam Goreng :".$ayam_goreng;
+echo "<li> Ayam Bakar :".$ayam_bakar;
+echo "<li> Ayam Sayur :".$ayam_sayur;
+echo "<li> Tempe Goreng :".$tempe_goreng;
+echo "<li> Tahu Goreng :".$tahu_goreng;
+echo "<li> Soto :".$soto;
+echo "<li> Nasi Putih :".$nasi_putih;
+echo "<li> Es teh :".$es_teh;
+echo "<li> Es Tebu :".$es_tebu;
+
+
+
+?>
